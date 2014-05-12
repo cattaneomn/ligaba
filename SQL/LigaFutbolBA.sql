984dfbbe60a4cc2406f8af94aed3088d51fcdeeb
Luciano <luciano.a.colucci@gmail.com>
2014-05-12 13:29:18 -0300

    Bug Tarjetas

--- SQL/LigaFutbolBA.sql
+++ SQL/LigaFutbolBA.sql
@@ -1255,8 +1255,13 @@ BEGIN transaction
         UPDATE LigaBA.Partido SET goleslocal=@golesLocal, golesvisiante=@golesVisitante
         WHERE id=@idPartido
         
-        --ACTUALIZO SUSPENDIDOS
-        UPDATE LigaBA.TorneoXCategoriaXJugador SET habilitado = 1,amarillas=0,rojas=0 WHERE habilitado = 0 AND (amarillas >= 5 OR rojas >= 1) 
+        --ACTUALIZO SUSPENDIDOS AMARRILLAS
+        UPDATE LigaBA.TorneoXCategoriaXJugador SET habilitado = 1,amarillas=0,rojas=0 WHERE habilitado = 0 AND amarillas >= 5
+        AND (jugador IN (SELECT JE.jugador FROM LigaBA.JugadorXEquipo as JE WHERE JE.equipo= @localId) 
+        OR jugador IN (SELECT JXE.jugador FROM LigaBA.JugadorXEquipo as JXE WHERE JXE.equipo= @visitanteId))
+        
+        --ACTUALIZO SUSPENDIDOS ROJAS
+        UPDATE LigaBA.TorneoXCategoriaXJugador SET habilitado = 1,rojas=0 WHERE habilitado = 0 AND rojas >= 1 
         AND (jugador IN (SELECT JE.jugador FROM LigaBA.JugadorXEquipo as JE WHERE JE.equipo= @localId) 
         OR jugador IN (SELECT JXE.jugador FROM LigaBA.JugadorXEquipo as JXE WHERE JXE.equipo= @visitanteId))
         
